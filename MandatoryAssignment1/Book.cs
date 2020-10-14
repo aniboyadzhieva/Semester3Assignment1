71e303615235faa57043303963057a1bfa50f3fd
aniboyadzhieva <anix0072@edu.easj.dk>
2020-10-14 15:32:53 +0200

    ...

---
+++ MandatoryAssignment1/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MandatoryAssignment1
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int pageNumber;
+        private string isbn13;
+
+
+        public Book(string title, string author, int pageNumber, string isbn13)
+        {
+            Title = title;
+            Author = author;
+            PageNumber = pageNumber;
+            Isbn13 = isbn13;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("Title must consist of two characters at least");
+                }
+
+                title = value;
+            }
+        }
+
+
+        public string Author { get; set; }
+
+        public int PageNumber
+        {
+            get { return pageNumber; }
+            set
+            {
+                if (value < 10 || value >= 1000)
+                {
+                    throw new ArgumentException("Page number should be no shorter than 10 and no longer or equal than 1000");
+                }
+
+                pageNumber = value;
+            }
+        }
+
+        public string Isbn13
+        {
+            get { return isbn13; }
+            set
+            {
+                if (value.Length < 13 || value.Length > 13)
+                {
+                    throw new ArgumentException("Isbn should be 13 characters long");
+                }
+
+                isbn13 = value;
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,90 @@
+using System;
+using MandatoryAssignment1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTtileMethodLongerThan2Char()
+        {
+            Book book1 = new Book("Captain Hook", "J. M. Barrie", 20, "0000000000000");
+            string actualResult = book1.Title;
+            Assert.AreEqual("Captain Hook", actualResult);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestTtileMethodShorterThan2Char()
+        {
+            Book book2 = new Book("N", "no", 20, "0000000000000");
+            string actualResult = book2.Title;
+            Assert.AreEqual(typeof(ArgumentException), actualResult);
+        }
+
+        [TestMethod]
+        public void TestPageNumberMethodRightAmount()
+        {
+            Book book3 = new Book("Sleeping Beauty", "Charles Perrault", 50, "0000000000000");
+            int actualResult = book3.PageNumber;
+            Assert.AreEqual(50, actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPageNumberMethodShorterThan10()
+        {
+            Book book4 = new Book("Ugly Duckling", "Hans Christian Andersen", 6, "0000000000000");
+            int actualResult = book4.PageNumber;
+            Assert.AreEqual(typeof(ArgumentException), actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPageNumberMethodLongerThan1000()
+        {
+            Book book5 = new Book("Snow White", "Grimms", 3000, "0000000000000");
+            int actualResult = book5.PageNumber;
+            Assert.AreEqual(typeof(ArgumentException), actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPageNumberMethodEqualsTo1000()
+        {
+            Book book6 = new Book("Man", "smth", 1000, "0000000000000");
+            int actualResult = book6.PageNumber;
+            Assert.AreEqual(typeof(ArgumentException), actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestIsbn13MethodShorterThan13()
+        {
+            Book book7 = new Book("Duck", "Smith", 60, "000000000");
+            string actualResult = book7.Isbn13;
+            Assert.AreEqual(typeof(ArgumentException), actualResult);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestIsbn13MethodLongerThan13()
+        {
+            Book book7 = new Book("Cat", "Rock", 65, "000000000000000000000");
+            string actualResult = book7.Isbn13;
+            Assert.AreEqual(typeof(ArgumentException), actualResult);
+        }
+
+        [TestMethod]
+        public void TestIsbn13MethodEqualsTo13()
+        {
+            Book book7 = new Book("Cat", "Rock", 65, "0000000000000");
+            string actualResult = book7.Isbn13;
+            Assert.AreEqual("0000000000000", actualResult);
+        }
+
+    }
+}
